2bf0be12cde3065a88734cc80e1e9074b8bd16d2
Alice Bartlett <alice.bartlett@ft.com>
2017-02-01 13:56:23 +0000

    Get basic Origami tests working

--- src/js/componentBoilerplate.js
+++ src/js/componentBoilerplate.js
@@ -1,9 +1,12 @@
-class ComponentBoilerplate {
+class InPageNav {
 
-	constructor (ComponentBoilerplateEl, opts) {
-		this.ComponentBoilerplateEl = ComponentBoilerplateEl;
-		this.opts = opts || {values: "default"};
+	constructor (InPageNavEl, opts) {
+		this.InPageNavEl = InPageNavEl;
+		this.opts = opts || {}; // no opts right now
 	}
+
+	<!-- nav code goes here -->
+
 
 	static init (rootEl, opts) {
 		if (!rootEl) {
@@ -12,11 +15,11 @@ class ComponentBoilerplate {
 		if (!(rootEl instanceof HTMLElement)) {
 			rootEl = document.querySelector(rootEl);
 		}
-		if (rootEl instanceof HTMLElement && /\bo-component-boilerplate\b/.test(rootEl.getAttribute('data-o-component'))) {
-			return new ComponentBoilerplate(rootEl, opts);
+		if (rootEl instanceof HTMLElement && /\bo-in-page-nav\b/.test(rootEl.getAttribute('data-o-component'))) {
+			return new InPageNav(rootEl, opts);
 		}
-		return Array.from(rootEl.querySelectorAll('[data-o-component="o-component-boilerplate"]'), rootEl => new ComponentBoilerplate(rootEl, opts));
+		return Array.from(rootEl.querySelectorAll('[data-o-component="o-in-page-nav"]'), rootEl => new InPageNav(rootEl, opts));
 	}
 }
 
-export default ComponentBoilerplate;
+export default InPageNav;
