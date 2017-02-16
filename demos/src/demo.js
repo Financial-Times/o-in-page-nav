01e9b93cb933ff97f1df577900f9bd0690dd3347
Alice Bartlett <alice.bartlett@ft.com>
2017-02-16 15:39:44 +0000

    improve demo

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -4,9 +4,10 @@ import './../../main.js';
 function initDemos() {
 	document.addEventListener('DOMContentLoaded', function() {
 
-		if (window.self === window.top) {
-			document.querySelector('.demo-notice').remove();
+		if (window.self !== window.top) {
+			document.querySelector('.demo-wrapper').classList.add('in-iframe');
 		}
+
 
 		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 
